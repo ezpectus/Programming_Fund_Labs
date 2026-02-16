46675185a189d124203a200a8b63d8a48cf77ecd
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-02-16 12:20:10 +0000

    Implemented infinite Fibonacci generator using yield

--- CourseProject/Generators(Lab1)/FibonacciGenerator.cs
+++ CourseProject/Generators(Lab1)/FibonacciGenerator.cs
@@ -1,6 +1,23 @@
+
+
+using System.Collections.Generic;
+
 namespace GeneratorsLab1;
 
 public static class FibonacciGenerator
 {
-    // TODO: Implement infinite Fibonacci generator
+    public static IEnumerable<long> Generate()
+    {
+        long a = 0;
+        long b = 1;
+
+        while (true)
+        {
+            yield return a;
+
+            long temp = a;
+            a = b;
+            b = temp + b;
+        }
+    }
 }
