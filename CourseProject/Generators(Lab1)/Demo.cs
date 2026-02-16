b4638af65d604a95a8c4cadd55669d24f6455fbd
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-02-16 14:28:39 +0000

    Added timeout iterator and connected it with Fibonacci generator

--- CourseProject/Generators(Lab1)/Demo.cs
+++ CourseProject/Generators(Lab1)/Demo.cs
@@ -1,12 +1,12 @@
 namespace GeneratorsLab1;
 
+namespace GeneratorsLab1;
+
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Generators Lab 1; Demo");
-        //Demo ilustrating the use of generators
-
-
+        var fib = FibonacciGenerator.Generate();
+        TimeoutIterator.Consume(fib, 5);
     }
 }
--- CourseProject/Generators(Lab1)/TimeoutIterator.cs
+++ CourseProject/Generators(Lab1)/TimeoutIterator.cs
@@ -1,6 +1,21 @@
+
+using System;
+using System.Collections.Generic;
+
 namespace GeneratorsLab1;
 
 public static class TimeoutIterator
 {
-    // TODO: Implement timeout iterator
+    public static void Consume<T>(IEnumerable<T> generator, int seconds)
+    {
+        var endTime = DateTime.Now.AddSeconds(seconds);
+
+        foreach (var item in generator)
+        {
+            if (DateTime.Now >= endTime)
+                break;
+
+            Console.WriteLine(item);
+        }
+    }
 }
