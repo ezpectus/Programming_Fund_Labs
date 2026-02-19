416deaa85ab0b39a88be693fc1aa11f06299fa40
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-02-19 12:03:47 +0000

    initial

---
+++ CourseProject/Generators(lab1)/FibonacciGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GeneratorsLab1
+{
+    public static class FibonacciGenerator
+    {
+        public static IEnumerable<long> Generate()
+        {
+            long a = 0;
+            long b = 1;
+
+            while (true)
+            {
+                yield return a;
+
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+        }
+    }
+}
