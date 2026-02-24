50eaaab765f27caaffd0ad68b6e1579d1d81f785
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-02-24 18:30:02 +0000

    Implemented peek:newest,oldest,lowest,highest;Some additional refactoring of lab4

--- CourseProject/BiDirQueueLab4/BiDirectionalPriorityQueue.cs
+++ CourseProject/BiDirQueueLab4/BiDirectionalPriorityQueue.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PGR_FUND_LABS_CS.CourseProject.BiDirQueueLab4
 {
     public class BiDirectionalPriorityQueue<T>
@@ -28,6 +29,64 @@ namespace PGR_FUND_LABS_CS.CourseProject.BiDirQueueLab4
             sequenceCounter = 0;
         }
         // Enqueue an item with a given priority
+
+        public void Enqueue(T item, int priority)
+        {
+            var entry = new Entry
+            {
+                Item = item,
+                Priority = priority,
+                Sequence = sequenceCounter++
+            };
+
+            var insertionNode = insertionOrder.AddLast(entry);
+            entry.InsertionNode = insertionNode;
+
+            if (!priorities.ContainsKey(priority))
+                {
+                    priorities[priority] = new LinkedList<Entry>();
+            }
+
+
+            var priorityNode = priorities[priority].AddLast(entry);
+            entry.PriorityNode = priorityNode;
+        }
+
+
+        public T peekOldest()
+        {
+            
+            if (insertionOrder.Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+            return insertionOrder.First.Value.Item;
+
+        }
+
+
+        public T peekNewest()
+        {
+
+            if (insertionOrder.Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+            return insertionOrder.Last.Value.Item;
+        }
+
+
+        public T peekHighest()
+        {
+            if (priorities.Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+            var highestPriority = priorities.Keys.Last();
+            return priorities[highestPriority].First.Value.Item;
+        }
+        public T peekLowest()
+        {
+              if (priorities.Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+            var lowestPriority = priorities.Keys.First();
+            return priorities[lowestPriority].First.Value.Item;
+        }
+
 
     }
 }
