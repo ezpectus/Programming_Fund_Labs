d1fbf0488228a5caa818632513c8a59fceb63c17
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-02-25 11:16:19 +0000

    add Lab5 async array project structure

---
+++ CourseProject/AsyncArray(Lab5)/AsyncArrayDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGR_FUND_LABS_CS.CourseProject.AsyncArray_Lab5_
+{
+    internal class AsyncArrayDemo
+    {
+    }
+}
