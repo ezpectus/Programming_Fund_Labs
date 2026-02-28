86690c2589299556c90dbfc9f8e16f9dab394eed
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-02-28 15:20:59 +0000

    Implemented async stream reader using IAsyncEnumerable

--- CourseProject/LargeDataProcessing/DataStreamReader.cs
+++ CourseProject/LargeDataProcessing/DataStreamReader.cs
@@ -1,23 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PGR_FUND_LABS_CS.CourseProject.LargeDataProcessing
 {
-    // Responsible for reading large files incrementally
+    // Reads large files incrementally using async streams
     public class DataStreamReader
     {
-        // TODO:
-        // Implement async iterator using IAsyncEnumerable<DataRecord>
-        // Should read file line by line without loading entire file into memory
-
-        public async IAsyncEnumerable<DataRecord> ReadAsync(string filePath)
+        public async IAsyncEnumerable<DataRecord> ReadAsync(
+            string filePath,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
-            // Implementation will be added in next commit
-            await Task.CompletedTask;
-            yield break;
+            using var stream = new FileStream(
+                filePath,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read,
+                bufferSize: 4096,
+                useAsync: true);
+
+            using var reader = new StreamReader(stream);
+            int id = 0;
+
+            while (!reader.EndOfStream)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                var line = await reader.ReadLineAsync();
+
+                if (line is null)
+                    continue;
+
+                yield return new DataRecord
+                {
+                    Id = id++,
+                    RawContent = line
+                };
+            }
         }
     }
 }
