1a67d0f2f537a2049a177197830ad2b259cd96c3
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-08 19:50:33 +0000

    feat: add Logger observer that logs values errors and completion to console

--- CourseProject/ReactiveCom(Lab7)/Entities/Logger.cs
+++ CourseProject/ReactiveCom(Lab7)/Entities/Logger.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PGR_FUND_LABS_CS.CourseProject.ReactiveCom_Lab7_.Core;
+using System;
+
 
 namespace PGR_FUND_LABS_CS.CourseProject.ReactiveCom_Lab7_.Entities
 {
-  public class Logger
+    public class Logger : IEventObserver<double>
     {
 
-
-
-
+     public void OnNext(double value)
+        {
+            Console.WriteLine($"Logger received value: {value}");
+        }
+        public void OnError(Exception ex)
+        {
+            Console.WriteLine($"Logger received error: {ex.Message}");
+        }
+        public void OnCompleted()
+        {
+            Console.WriteLine("Logger received completion signal.");
+        }
 
     }
 }
