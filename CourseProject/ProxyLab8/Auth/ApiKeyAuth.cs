1f3eda5424949ef03d48f8983a5765fec0c24702
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-10 14:26:01 +0000

    feat: add ApiKeyAuth strategy that injects API key into request headers

--- CourseProject/ProxyLab8/Auth/ApiKeyAuth.cs
+++ CourseProject/ProxyLab8/Auth/ApiKeyAuth.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Net.Http;
+using PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Core;
 
-namespace PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Auth
+
+namespace PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Auth // This class implements the IAuthStrategy interface to provide API key authentication for HTTP requests.
+                                                        // It adds the API key to the request headers under the "X-API-Key" header name.
 {
-    internal class ApiKeyAuth
+    public class ApiKeyAuth : IAuthStrategy
     {
+        private readonly string _apiKey;
+        public ApiKeyAuth(string apiKey)
+        {
+           _apiKey = apiKey;
+        }
+
+       public void Apply(HttpRequestMessage request) 
+        {
+            request.Headers.Add("X-API-Key", _apiKey);
+
+        }
     }
 }
+
