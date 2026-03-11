db62bd9cf976c9df85ef53d3435dcee394681b31
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-11 11:29:27 +0000

    feat: add AuthProxy that applies auth strategy before forwarding requests

--- CourseProject/ProxyLab8/Core/AuthProxy.cs
+++ CourseProject/ProxyLab8/Core/AuthProxy.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
+
+
 
 namespace PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Core
 {
-    internal class AuthProxy
+     public class AuthProxy : IApiService
     {
+        private readonly IApiService _apiService;
+        private readonly IAuthStrategy _authStrategy;
+
+        public AuthProxy(IApiService apiService, IAuthStrategy authStrategy)
+        {
+            _apiService = apiService;
+            _authStrategy = authStrategy;
+        }
+      
+        public async Task<string> SendAsync(HttpRequestMessage request)
+        {
+             _authStrategy.Apply(request);
+           return await _apiService.SendAsync(request);
+        }
+
+
     }
 }
