76f2c632cf494c2852c322ca80ab3769aedbaea6
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-11 11:35:37 +0000

    feat: add RealApiService that simulates HTTP request with network delay

--- CourseProject/ProxyLab8/DemoLab8.cs
+++ CourseProject/ProxyLab8/DemoLab8.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Auth;
+using PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Service;
+using PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Core;
+
 
 namespace PGR_FUND_LABS_CS.CourseProject.ProxyLab8
 {
-    internal class DemoLab8
+   public class DemoLab8
     {
+
+         
+
+
+
     }
 }
--- CourseProject/ProxyLab8/Service/RealApiService.cs
+++ CourseProject/ProxyLab8/Service/RealApiService.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
+using PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Core;
 
 namespace PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Service
 {
-    internal class RealApiService
+    public class RealApiService : IApiService
     {
+        private readonly string _baseUrl;
+
+        public RealApiService(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+        }
+
+        public async Task<string> SendAsync(HttpRequestMessage request)
+        {
+            // Simulate sending the request to the real API and getting a response
+            Console.WriteLine($"Sending request to {_baseUrl}{request.RequestUri}");
+            await Task.Delay(500); // Simulate network delay
+            return $"Response from {_baseUrl}{request.RequestUri}";
+
+
+        }
     }
 }
