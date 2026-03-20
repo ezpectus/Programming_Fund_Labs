7f7b3fb93afdcb302700f2037231e0b0f9c60d18
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-20 22:33:04 +0000

    feat: add LogLevel enum and ILogger interface for Lab9 core

--- CourseProject/LoggerLab9/CoreLab9/ILogger.cs
+++ CourseProject/LoggerLab9/CoreLab9/ILogger.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PGR_FUND_LABS_CS.CourseProject.LoggerLab9.CoreLab9
 {
-    internal interface ILogger
+    public interface ILogger
     {
+        void Log(LogLevel level, string message);
+        bool IsEnabled(LogLevel level);
     }
 }
