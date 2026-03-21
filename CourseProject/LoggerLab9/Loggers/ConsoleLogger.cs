f4cdbbec1a5bb5ad0b5c54ae68fc924f9ae3372c
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-21 22:17:29 +0000

    feat: add ConsoleLogger that filters and formats log messages by level

--- CourseProject/LoggerLab9/Loggers/ConsoleLogger.cs
+++ CourseProject/LoggerLab9/Loggers/ConsoleLogger.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PGR_FUND_LABS_CS.CourseProject.LoggerLab9.CoreLab9;
 
 namespace PGR_FUND_LABS_CS.CourseProject.LoggerLab9.Loggers
 {
-    internal class ConsoleLogger
+   public class ConsoleLogger : ILogger 
     {
+        private readonly LogLevel _minlogLevel;
+        private readonly ILogFormatter _logFormatter;
+        public ConsoleLogger(LogLevel minlogLevel, ILogFormatter logFormatter)
+        {
+            _minlogLevel = minlogLevel;
+            _logFormatter = logFormatter;
+        }
+        public bool IsEnabled(LogLevel level) => level >= _minlogLevel;
+        public void Log(LogLevel level, string message)
+        {
+            if (!IsEnabled(level))
+                return;
+            var formattedMessage = _logFormatter.Format(level, message, DateTime.Now);
+            Console.WriteLine(formattedMessage);
+        }
+
     }
 }
