c7d946001f100a91145be3cdb99d546a345bf33d
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-21 22:39:39 +0000

    feat: add FileLogger with formatter support and ClearLog method

--- CourseProject/LoggerLab9/Loggers/FileLogger.cs
+++ CourseProject/LoggerLab9/Loggers/FileLogger.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using PGR_FUND_LABS_CS.CourseProject.LoggerLab9.CoreLab9;
 
 namespace PGR_FUND_LABS_CS.CourseProject.LoggerLab9.Loggers
 {
-    internal class FileLogger
+    public class FileLogger : ILogger
     {
+        private readonly string _filePath;
+        private readonly LogLevel _minLogLevel;
+        private readonly ILogFormatter _formatter;
+
+        public FileLogger(string filePath, LogLevel minLogLevel, ILogFormatter formatter)
+        {
+            _filePath = filePath;
+            _minLogLevel = minLogLevel;
+            _formatter = formatter;
+        }
+
+        public bool IsEnabled(LogLevel level) => level >= _minLogLevel;
+
+        public void Log(LogLevel level, string message)
+        {
+            if (!IsEnabled(level)) return;
+            var logEntry = _formatter.Format(level, message, DateTime.Now);
+            File.AppendAllText(_filePath, logEntry + Environment.NewLine);
+        }
+
+        public void ClearLog()
+        {
+            File.WriteAllText(_filePath, string.Empty);
+        }
     }
 }
+
+
