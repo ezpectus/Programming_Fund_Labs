1cb603a44f7d9d05ebfb0d02eeac85fd13e3d171
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-22 20:04:56 +0000

    feat: add LoggingDecorator with Execute and ExecuteAsync methods for sync and async logging

--- CourseProject/LoggerLab9/CoreLab9/LoggingDecorator.cs
+++ CourseProject/LoggerLab9/CoreLab9/LoggingDecorator.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+
+using System;
 using System.Threading.Tasks;
 
 namespace PGR_FUND_LABS_CS.CourseProject.LoggerLab9.CoreLab9
 {
-    internal class LoggingDecorator
+   public class LoggingDecorator
     {
+        private readonly ILogger _logger;
+        public LoggingDecorator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public T Execute<T>(Func<T> action, LogLevel level, string methodName)
+        {
+            _logger.Log(level, $"Executing: {methodName}");
+            var start = DateTime.Now;
+            var result = action();
+            var duration = DateTime.Now - start;
+            _logger.Log(level, $"Result: {result}, Time: {duration.TotalMilliseconds}ms");
+            return result;
+        }
+       
+        public async Task<T> ExecuteAsync<T>(Func<Task<T>> action, LogLevel level, string methodName)
+        {
+            _logger.Log(level, $"Executing: {methodName}");
+            var start = DateTime.Now;
+            var result = await action();
+            var duration = DateTime.Now - start;
+            _logger.Log(level, $"Result: {result}, Time: {duration.TotalMilliseconds}ms");
+            return result;
+        }
+
     }
 }
--- CourseProject/LoggerLab9/Formatters/PlainFormatter.cs
+++ CourseProject/LoggerLab9/Formatters/PlainFormatter.cs
@@ -5,7 +5,6 @@ namespace PGR_FUND_LABS_CS.CourseProject.LoggerLab9.Formatters
 {
     public  class PlainFormatter : ILogFormatter
     {
-
         public string Format(LogLevel level, string message, DateTime timestamp)
         {
             return $"[{timestamp:yyyy-MM-dd HH:mm:ss}] {level}: {message}";
