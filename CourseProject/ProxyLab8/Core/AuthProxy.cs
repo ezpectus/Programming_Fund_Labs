e22a441dbd7e3f0b78b721b4e1ab6862ee00520a
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-05-05 23:28:16 +0100

    added logging to proxy execution flow

--- CourseProject/ProxyLab8/Core/AuthProxy.cs
+++ CourseProject/ProxyLab8/Core/AuthProxy.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Core
@@ -16,7 +17,10 @@ namespace PGR_FUND_LABS_CS.CourseProject.ProxyLab8.Core
 
         public async Task<string> SendAsync(HttpRequestMessage request)
         {
+            Console.WriteLine("[Proxy] Applying authentication...");
             _authStrategy.Apply(request);
+
+            Console.WriteLine("[Proxy] Forwarding request...");
             return await _apiService.SendAsync(request);
         }
     }
