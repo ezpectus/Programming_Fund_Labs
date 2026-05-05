644430f3bfdcbb689bdee2448dd081670ebbfd49
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-05-05 23:13:22 +0100

    Improve callback-based async map stability
    - Fixed cancellation handling to avoid hanging
    - Added basic error handling
    - Minor improvements for reliability

--- CourseProject/AsyncArray(Lab5)/AsyncArrayCallback.cs
+++ CourseProject/AsyncArray(Lab5)/AsyncArrayCallback.cs
@@ -6,17 +6,16 @@ using System.Threading.Tasks;
 using System.Threading;
 
 
-
 namespace PGR_FUND_LABS_CS.CourseProject.AsyncArray_Lab5_
 {
     public static class AsyncMapCallback
     {
         public static void MapAsync<T, TResult>(
-            IList<T> source,
-            Func<T, Action<TResult>, CancellationToken, Task> selector,
-            Action<List<TResult>> onCompleted,
-            CancellationToken token)
-        {
+          IList<T> source,
+          Func<T, Action<TResult>, CancellationToken, Task> selector,
+          Action<List<TResult>> onCompleted,
+          CancellationToken token)
+           {
             var results = new TResult[source.Count];
             int completed = 0;
 
@@ -26,19 +25,28 @@ namespace PGR_FUND_LABS_CS.CourseProject.AsyncArray_Lab5_
 
                 Task.Run(async () =>
                 {
-                    if (token.IsCancellationRequested)
-                        return;
-
-                    await selector(source[index], result =>
+                    try
                     {
-                        results[index] = result;
-
-                        if (Interlocked.Increment(ref completed) == source.Count)
+                        if (token.IsCancellationRequested)
                         {
-                            onCompleted(new List<TResult>(results));
+                            if (Interlocked.Increment(ref completed) == source.Count)
+                                onCompleted(new List<TResult>(results));
+                            return;
                         }
 
-                    }, token);
+                        await selector(source[index], result =>
+                        {
+                            results[index] = result;
+
+                            if (Interlocked.Increment(ref completed) == source.Count)
+                                onCompleted(new List<TResult>(results));
+                        }, token);
+                    }
+                    catch
+                    {
+                        if (Interlocked.Increment(ref completed) == source.Count)
+                            onCompleted(new List<TResult>(results));
+                    }
 
                 }, token);
             }
